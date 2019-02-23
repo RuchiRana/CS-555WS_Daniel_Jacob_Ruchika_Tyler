a8f1b03f0143b956e1f47dd18a1fdd40d45ebd4a
RuchiRana <ruchika123rana@gmail.com>
2019-02-23 14:40:44 -0500

    Project 4

---
+++ Project/Project/src/Project4/Driver.java
@@ -0,0 +1,12 @@
+package Project4;
+
+import java.io.IOException;
+
+public class Driver
+{
+	public static void main(String[] args) throws IOException
+	{
+		Project project04 = new Project("resources/tags.txt","resources/Project04.ged");
+		project04.run();
+	}
+}
