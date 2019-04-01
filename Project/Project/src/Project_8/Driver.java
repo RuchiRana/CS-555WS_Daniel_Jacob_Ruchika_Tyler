04fd6eef65f4534bc7034682f66e0fe5c834367b
RuchiRana <ruchika123rana@gmail.com>
2019-03-31 20:07:50 -0400

    Project 8

---
+++ Project/Project/src/Project_8/Driver.java
@@ -0,0 +1,14 @@
+package Project_8;
+
+import java.io.IOException;
+import java.text.ParseException;
+
+public class Driver
+{
+	public static void main(String[] args) throws IOException, ParseException
+	{
+		Project project06 = new Project("resources/tags.txt","resources/testFile.ged");
+		
+		project06.run();
+	}
+} 
